5c9bb60e245c06a809c352f1df5ab7e8d58a9652
Elagin Pavel <elagin.pasha@gmail.com>
2015-04-07 17:14:09 +0300

    Еще раз.

--- Motocitizen/src/motocitizen/app/mc/MCObjects.java
+++ Motocitizen/src/motocitizen/app/mc/MCObjects.java
@@ -17,8 +17,6 @@ public class MCObjects {
     //public static final Button authButton = (Button) findView(R.id.app_mcaccidents_register_button);
     //public static final Button authConfirmButton = (Button) findView(R.id.mc_auth_confirm_button);
     //public static final Button authCancelButton = (Button) findView(R.id.mc_auth_cancel_button);
-    // zz
-    // public static final Button newMessageButton = (Button) findView(R.id.mc_new_message_send);
     public static final ImageButton dialButton = (ImageButton) findView(R.id.dial_button);
     public static final ImageButton createAccButton = (ImageButton) findView(R.id.mc_add_point_button);
     public static final CheckBox authAnonimCheckBox = (CheckBox) findView(R.id.mc_auth_anonim);
