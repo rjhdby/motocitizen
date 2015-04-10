b4d5555cfa2738bec3626fb394035a1b62d51453
Pavel Elagin <elagin.pasha@gmail.com>
2015-04-10 03:21:19 +0300

    ActionBar, back navigation

--- Motocitizen/src/motocitizen/Activity/AboutActivity.java
+++ Motocitizen/src/motocitizen/Activity/AboutActivity.java
@@ -1,23 +1,44 @@
 package motocitizen.Activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import motocitizen.main.R;
 
 public class AboutActivity extends ActionBarActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_about);
-  }
+//Посмотреть http://android-developers.blogspot.in/2013/08/actionbarcompat-and-io-2013-app-source.html
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    //getMenuInflater().inflate(R.menu.menu_about, menu);
-    return true;
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuitem_search:
+                Toast.makeText(this, getString(R.string.ui_menu_search),
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menuitem_send:
+                Toast.makeText(this, getString(R.string.ui_menu_send),
+                        Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
 }
