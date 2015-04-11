57096126098c74033f6c80cd6efe5cc7fedc7f60
rjhdby <andrewgrom@rambler.ru>
2015-04-11 23:38:46 +0300

    Добавил в about номер текущей версии кода https://github.com/rjhdby/motocitizen/issues/119

--- Motocitizen/src/motocitizen/Activity/AboutActivity.java
+++ Motocitizen/src/motocitizen/Activity/AboutActivity.java
@@ -1,8 +1,11 @@
 package motocitizen.Activity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.widget.TextView;
 
 import motocitizen.main.R;
 
@@ -12,6 +15,16 @@ public class AboutActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        PackageManager manager = this.getPackageManager();
+        String version;
+        try {
+            PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+            version = getString(R.string.code_version_prefix) + ": " + info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            version = getString(R.string.unknown_code_version);
+        }
+
+        ((TextView) this.findViewById(R.id.about_code_version)).setText(version);
     }
 
     @Override
