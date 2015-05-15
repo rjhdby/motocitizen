8f75c9d20b7ebf9e6f153cdbd9d02b8f155b2e32
Pavel Elagin <elagin.pasha@gmail.com>
2015-05-15 19:56:35 +0300

    Подкладываем старый Layout

--- Motocitizen/src/motocitizen/Activity/CreateAccActivityNewFragment.java
+++ Motocitizen/src/motocitizen/Activity/CreateAccActivityNewFragment.java
@@ -19,6 +19,6 @@ public class CreateAccActivityNewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_create_acc_activity_new, container, false);
+        return inflater.inflate(R.layout.mc_app_create_point, container, false);
     }
 }
