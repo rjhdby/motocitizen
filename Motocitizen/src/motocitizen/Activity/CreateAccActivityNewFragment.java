1c3adf77c80aef787234a9a8a093722d198c2c0c
Pavel Elagin <elagin.pasha@gmail.com>
2015-05-16 01:57:05 +0300

    Подставляем старый layout

--- Motocitizen/src/motocitizen/Activity/CreateAccActivityNewFragment.java
+++ Motocitizen/src/motocitizen/Activity/CreateAccActivityNewFragment.java
@@ -5,20 +5,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import motocitizen.main.R;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class CreateAccActivityNewFragment extends Fragment {
+public class CreateAccActivityNewFragment extends Fragment implements View.OnClickListener {
+
+    private Button cancel;
 
     public CreateAccActivityNewFragment() {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.mc_app_create_point, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //View viewMain = inflater.inflate(R.layout.mc_app_create_point, container, false);
+        View viewMain = inflater.inflate(R.layout.mc_app_create_point, container, false);
+
+        cancel = (Button) viewMain.findViewById(R.id.mc_create_cancel);
+        cancel.setOnClickListener(this);
+
+        return viewMain;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.mc_create_cancel:
+                getActivity().finish();
+                break;
+        }
     }
 }
